7e55a08d4f36a6b4c1c7f206dd83fb409d5b46c7
Tony <jyang13@ualberta.ca>
2020-07-23 00:55:41 -0600

    "attempting to make my life work :')"

--- app/hello.py
+++ app/hello.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class grocery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    groceryName = db.Column(db.String(200), unique=True, nullable=False)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Food %r>' % self.id 
+
+
+@app.route('/')
 def home():
     return render_template('home.html')
 
